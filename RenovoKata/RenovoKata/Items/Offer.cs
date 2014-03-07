733d0f4584476419e8c60008f9b2316a32f8f4cd
SamGraber <samgraber@sundialproperties.com>
2014-03-07 10:07:15 -0500

    Added Offer class.

---
+++ RenovoKata/RenovoKata/Items/Offer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RenovoKata.Items
+{
+	public class Offer
+	{
+		public Offer(int numItems, int price)
+		{
+			NumItems = numItems;
+			OfferPrice = price;
+		}
+
+		public int NumItems { get; set; }
+
+		public int OfferPrice { get; set; }
+	}
+}
