b9202f5e363d0e0295382889475b0c66b2eabe39
SamGraber <samgraber@sundialproperties.com>
2014-03-07 10:30:33 -0500

    Added base logic for initializing items for purchase.

--- RenovoKata/RenovoKata/Cashier.cs
+++ RenovoKata/RenovoKata/Cashier.cs
@@ -2,10 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using RenovoKata.Items;
 
 namespace RenovoKata
 {
 	public class Cashier
 	{
+		public void PurchaseItem(string itemID)
+		{
+			Item i;
+
+			switch (itemID)
+			{
+				case "A":
+					i = new A();
+					break;
+				case "B":
+					i = new B();
+					break;
+				case "C":
+					i = new C();
+					break;
+				case "D":
+					i = new D();
+					break;
+				default:
+					i = new Item();
+					break;
+			}
+		}
 	}
 }
