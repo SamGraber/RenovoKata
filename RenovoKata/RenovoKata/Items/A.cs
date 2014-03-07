82ce1c451f54a3eea860c525c42306ce1b63af33
SamGraber <samgraber@sundialproperties.com>
2014-03-07 11:01:25 -0500

    Added constant price value and override GetPrice() function to A.

--- RenovoKata/RenovoKata/Items/A.cs
+++ RenovoKata/RenovoKata/Items/A.cs
@@ -7,5 +7,19 @@ namespace RenovoKata.Items
 {
 	public class A : Item
 	{
+		const int PRICE = 50;
+
+		public override int GetPrice(int itemNumber)
+		{
+			if (Offer != null)
+			{
+				if ((itemNumber % Offer.NumItems) == 0)
+				{
+					return Offer.OfferPrice - ((Offer.NumItems - 1) * PRICE);
+				}
+			}
+
+			return PRICE;
+		}
 	}
 }
