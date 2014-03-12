8d6b303c8f3719435256cac0d745601153e14197
SamGraber <samgraber@sundialproperties.com>
2014-03-12 15:12:09 -0400

    Added new Price class.

---
+++ RenovoKata/RenovoKata/Items/Price.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RenovoKata.Items
+{
+	public class Price
+	{
+		public Price(string itemType, int price)
+		{
+			ItemType = itemType;
+			Price = price;
+		}
+
+		public string ItemType { get; set; }
+
+		public int Price { get; set; }
+	}
+}
